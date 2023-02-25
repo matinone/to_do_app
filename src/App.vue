be73e9667899c9f79d26e8748911bdbdc3004572
mbrignone <mnbrignone@gmail.com>
2023-02-25 19:38:13 -0300

    Use menu instead of navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar title="To-Do App" color="blue-grey-darken-1">
+    <v-app-bar color="blue-grey-darken-1">
       <template v-slot:prepend>
-        <v-app-bar-nav-icon @click.prevent="showDrawer = !showDrawer"></v-app-bar-nav-icon>
+        <v-btn icon>
+          <v-icon>mdi-menu</v-icon>
+          <v-menu activator="parent">
+            <v-list>
+              <v-list-item
+                v-for="item in navItems"
+                :key="item.title"
+                :title="item.title"
+                :value="item.value"
+                @click="navigate(item.value)"
+              >
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
       </template>
+
+      <v-app-bar-title>To-Do App</v-app-bar-title>
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
@@ -14,28 +30,13 @@
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-if="showDrawer" color="blue-grey-lighten-3" class="w-auto">
-      <v-list nav>
-        <v-list-item
-          v-for="item in navItems"
-          :key="item.title"
-          :title="item.title"
-          :value="item.value"
-          @click="navigate(item.value)"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-main>
-      <div class="text-h3 text-center">TASKS</div>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const showDrawer = ref(false);
 const navItems = [
   { title: "Tasks", value: "tasks" },
   { title: "Profile", value: "profile" }
